a82d26e8047fca18883426823ef258c0db3987df
jose-Abel <joseabel.ramirezfrontany@gmail.com>
2022-05-30 20:52:53 -0400

    Create the App and React components.

---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Greeting extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Hello World from React</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Greeting
